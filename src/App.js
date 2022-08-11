583fd7e5c451c4afa8a8ff504eb42fb77f28c3e2
cjh4549 <cjh4549@gmail.com>
2022-08-11 15:53:33 -0400

    configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="mt-20 ml-20">
+        hello
       </header>
     </div>
   );
