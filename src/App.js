4fa2920f8745cbd7a32b340bcdc4b5d0878ea310
cjh4549 <cjh4549@gmail.com>
2022-08-11 15:59:31 -0400

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './index.css';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="mt-20 ml-20">
-        hello
-      </header>
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    Navbar
+                </ul>
+            </nav>
+        </header>
+    )
+}
