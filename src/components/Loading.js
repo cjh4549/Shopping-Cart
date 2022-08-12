4ea5dc67e57bb1e41b7b36ac66b4d5755b45eebf
cjh4549 <cjh4549@gmail.com>
2022-08-12 16:09:11 -0400

    created loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import { TailSpin } from 'react-loader-spinner';
+
+export default function Loading(){
+    return (
+        <div className="flex justify-center items-center">
+            <TailSpin type="Puff" color="#00BFFF" height={550} width={80} />
+        </div>
+    )
+}
