abc4f189ff8ba2500be0ae9ee3c1a29691cc8a49
cjh4549 <cjh4549@gmail.com>
2022-08-13 01:48:22 -0400

    rendered products UI on the main

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import { useProductContext } from '../context/ProductContextProvider';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 
 export default function ProductList(){
@@ -16,7 +17,21 @@ export default function ProductList(){
     } else {
         return(
             <section>
-                Product List
+                <h2>Products</h2>
+                <div>
+                    {data.map(({ id, title, image, rating:{ rate }, price }) => (
+                        <Link to={`/product/${id}`}>
+                            <article key={id}>
+                                <img src={image} alt={title} />
+                                <div>
+                                    <h4>{title}</h4>
+                                    <p>Rating: {rate}/5</p>
+                                    <p>${price}</p>
+                                </div>
+                            </article>
+                        </Link>
+                    ))}
+                </div>
             </section>
         )
     }
